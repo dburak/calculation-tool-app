e9ed620e28377746f4ece8777ace9a767915e6fd
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-08-18 00:45:36 +0300

    production bug search #1

--- backend/middleware/auth-check.js
+++ backend/middleware/auth-check.js
@@ -3,6 +3,7 @@ const config = require('../utils/config');
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization');
+  console.log(authorization);
   if (authorization && authorization.startsWith('Bearer ')) {
     request.token = authorization.replace('Bearer ', '');
     next();
